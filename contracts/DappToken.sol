e2724b489f3c09e0cf992b8eac20e41741e940da
Gregory McCubbin <gwmccubbin@gmail.com>
2018-02-19 17:02:26 -0600

    Implements transfer function for Dapp Token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -5,15 +5,33 @@ contract DappToken {
     string public symbol = "DAPP";
     string public standard = "DApp Token v1.0";
     uint8 public decimals = 18;
-
-    address public admin;
     uint256 public totalSupply;
 
     mapping (address => uint256) public balanceOf;
+    // Allowance Mapping
+
+    // Transfer Event
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    // Approval Event
 
     function DappToken (uint256 _initialSupply) public {
-        admin = msg.sender;
-        balanceOf[admin] = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
+        // Trigger a transfer event when deploying
     }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+    // Approve function
+    // Transfer From function
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -3,7 +3,7 @@ var DappToken = artifacts.require('./DappToken.sol');
 contract('DappToken', function(accounts) {
   var tokenInstance;
 
-  it('Initializes the contract with the correct config values', function() {
+  it('initializes the contract with the correct config values', function() {
     return DappToken.deployed().then(function(instance) {
       tokenInstance = instance;
       return tokenInstance.name();
@@ -21,18 +21,44 @@ contract('DappToken', function(accounts) {
     });
   });
 
-  it('It allocates the initial token supply upon deployment', function() {
+  it('allocates the initial token supply upon deployment', function() {
     return DappToken.deployed().then(function(instance) {
       tokenInstance = instance;
       return tokenInstance.totalSupply();
     }).then(function(totalSupply) {
       assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to the initial supply');
-      return tokenInstance.admin()
-    }).then(function(admin) {
-      assert.notEqual(admin, 0x0, 'it stores the contract admin address');
-      return tokenInstance.balanceOf(admin);
+      return tokenInstance.balanceOf(accounts[0]);
     }).then(function(adminBalance) {
       assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial supply to the admin account');
     });
+  });
+
+  it('transfers token ownership', function() {
+    return DappToken.deployed().then(function(instance) {
+      tokenInstance = instance;
+      // Test `require` statement first by transferring something larger than the sender's balance
+      return tokenInstance.transfer.call(accounts[1], 999999999999999999);
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+      // This will only call the function, and not write to disk
+      // We want to inspect the function return value rather than the transaction receipt
+      return tokenInstance.transfer.call(accounts[1], 250000);
+    }).then(function(success) {
+      assert.equal(success, true, 'it returns true')
+      // Actually transfer by calling the function & writing to disk
+      return tokenInstance.transfer(accounts[1], 250000);
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+      assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
+      assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
+      assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+      return tokenInstance.balanceOf(accounts[0]);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 750000, 'deducts the amount from the sending account');
+      return tokenInstance.balanceOf(accounts[1]);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 250000, 'adds the amount to the receiving account');
+    })
   });
 });
