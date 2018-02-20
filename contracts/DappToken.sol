a66e4e4d1a1146d0f0db2d1e1394fc50dac26161
Gregory McCubbin <gwmccubbin@gmail.com>
2018-02-19 18:27:45 -0600

    Adds  function to token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.4.17;
 
 contract DappToken {
-    string public name = "DApp Token";
-    string public symbol = "DAPP";
-    string public standard = "DApp Token v1.0";
-    uint8 public decimals = 18;
+    string  public name = "DApp Token";
+    string  public symbol = "DAPP";
+    string  public standard = "DApp Token v1.0";
+    uint8   public decimals = 18;
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
@@ -25,21 +25,38 @@ contract DappToken {
     function DappToken (uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
-        // Trigger a transfer event when deploying
+        // TODO: Trigger a transfer event when deploying
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
+
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
+
         Transfer(msg.sender, _to, _value);
+
         return true;
     }
 
     function approve(address _spender, uint256 _value) public returns (bool success) {
         allowance[msg.sender][_spender] = _value;
+
         Approval(msg.sender, _spender, _value);
+
         return true;
     }
-    // Transfer From function
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+
+        Transfer(_from, _to, _value);
+
+        return true;
+    }
 }
