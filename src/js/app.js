50d57cbb2b5ec700fa4a8a4cfd58f7efba00dbc8
Gregory McCubbin <gwmccubbin@gmail.com>
2018-02-21 12:25:24 -0600

    WIP Successfully purchases tokens from client app. TODO: watch events successfully.

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ App = {
   account: '0x0',
   loading: false,
   hasVoted: false,
+  tokenPrice: 1000000000000000, // in wei
 
   init: function() {
     return App.initWeb3();
@@ -20,16 +21,7 @@ App = {
       web3 = new Web3(App.web3Provider);
     }
 
-    App.initContracts();
-
-    // Load account data
-    web3.eth.getCoinbase(function(err, account) {
-      console.log("Account address:", account)
-      if (err === null) {
-        App.account = account;
-        $("#accountAddress").html("Your Account: " + account);
-      }
-    });
+    return App.initContracts();
   },
 
   initContracts: function() {
@@ -46,32 +38,35 @@ App = {
         App.contracts.DappToken.deployed().then(function(dappToken) {
           console.log("Dapp Token Address:", dappToken.address);
         });
-        // App.listenForEvents();
-        // return App.render()
+        App.listenForEvents();
+        return App.render();
       });
     });
   },
 
   // Listen for events emitted from the contract
   listenForEvents: function() {
-    App.contracts.Election.deployed().then(function(instance) {
+    App.contracts.DappTokenSale.deployed().then(function(instance) {
       // Restart Chrome if you are unable to receive this event
       // This is a known issue with Metamask
       // https://github.com/MetaMask/metamask-extension/issues/2393
-      instance.votedEvent({}, {
+      instance.Sell({}, {
         fromBlock: 0,
         toBlock: 'latest'
       }).watch(function(error, event) {
         console.log("event triggered", event)
-        // Reload when a new vote is recorded
-        // App.render();
-
-
+        // Reload when tokens sold
+        App.render();
       });
     });
   },
 
   render: function() {
+    if (App.loading) {
+      return;
+    }
+    App.loading = true;
+
     var electionInstance;
     var loader = $("#loader");
     var content = $("#content");
@@ -88,54 +83,60 @@ App = {
     });
 
     // Load contract data
-    App.contracts.Election.deployed().then(function(instance) {
-      electionInstance = instance;
-      return electionInstance.candidatesCount();
-    }).then(function(candidatesCount) {
-      console.log("candidates count", candidatesCount.toNumber())
-      var candidatesResults = $("#candidatesResults");
-      candidatesResults.empty();
+    // App.contracts.DappTokenSale.deployed().then(function(instance) {
+    //   electionInstance = instance;
+    //   return electionInstance.candidatesCount();
+    // }).then(function(candidatesCount) {
+    //   console.log("candidates count", candidatesCount.toNumber())
+    //   var candidatesResults = $("#candidatesResults");
+    //   candidatesResults.empty();
 
-      var candidatesSelect = $('#candidatesSelect');
-      candidatesSelect.empty();
+    //   var candidatesSelect = $('#candidatesSelect');
+    //   candidatesSelect.empty();
 
-      for (var i = 1; i <= candidatesCount; i++) {
-        electionInstance.candidates(i).then(function(candidate) {
-          var id = candidate[0];
-          var name = candidate[1];
-          var voteCount = candidate[2];
+    //   for (var i = 1; i <= candidatesCount; i++) {
+    //     electionInstance.candidates(i).then(function(candidate) {
+    //       var id = candidate[0];
+    //       var name = candidate[1];
+    //       var voteCount = candidate[2];
 
-          // Render candidate Result
-          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
-          candidatesResults.append(candidateTemplate);
+    //       // Render candidate Result
+    //       var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+    //       candidatesResults.append(candidateTemplate);
 
-          // Render candidate ballot option
-          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
-          candidatesSelect.append(candidateOption);
-        });
-      }
-      return electionInstance.voters(App.account);
-    }).then(function(hasVoted) {
-      // Do not allow a user to vote
-      if(hasVoted) {
-        $('form').hide();
-      }
-      loader.hide();
-      content.show();
-    }).catch(function(error) {
-      console.warn(error);
-    });
+    //       // Render candidate ballot option
+    //       var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+    //       candidatesSelect.append(candidateOption);
+    //     });
+    //   }
+    //   return electionInstance.voters(App.account);
+    // }).then(function(hasVoted) {
+    //   // Do not allow a user to vote
+    //   if(hasVoted) {
+    //     $('form').hide();
+    //   }
+    //   // loader.hide();
+    //   // content.show();
+    // }).catch(function(error) {
+    //   console.warn(error);
+    // });
+    App.loading = false;
+    loader.hide();
+    content.show();
   },
 
-  castVote: function() {
-    var candidateId = $('#candidatesSelect').val();
-    App.contracts.Election.deployed().then(function(instance) {
-      return instance.vote(candidateId, {
+  buyTokens: function() {
+    var numberOfTokens = $('#numberOfTokens').val();
+    console.log("buying tokens...", numberOfTokens);
+    App.contracts.DappTokenSale.deployed().then(function(instance) {
+      return instance.buyTokens(numberOfTokens, {
         from: App.account,
+        value: numberOfTokens * App.tokenPrice,
         gas: 500000 // Gas limit
       });
     }).then(function(result) {
-      // Wait for votes to update
+      console.log("Bought tokens. Waiting for events...");
+      // Wait for events
       $("#content").hide();
       $("#loader").show();
     }).catch(function(err) {
